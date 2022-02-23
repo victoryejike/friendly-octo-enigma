d28edb5030ef36851e566e2c290c99e15e0d0991
Ejike Victory <victoryejike@gmail.com>
2022-02-23 11:52:48 +0100

    feat: react-context + file structure

---
+++ react-context-api/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './container/Header';
+import Auth from './container/Auth';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Auth />
+    </div>
+  );
+}
+
+export default App;
