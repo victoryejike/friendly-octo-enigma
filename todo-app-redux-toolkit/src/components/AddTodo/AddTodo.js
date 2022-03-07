0fa54c12143029371743885a03dddebd91635ddc
Ejike Victory <victoryejike@gmail.com>
2022-03-07 18:13:19 +0100

    feat: added some functionality to the input and delete icons

--- todo-app-redux-toolkit/src/components/AddTodo/AddTodo.js
+++ todo-app-redux-toolkit/src/components/AddTodo/AddTodo.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AddTodo.css';
 
 const AddTodo = () => {
+  const [todo, setTodo] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (todo === '') alert('please add a new task');
+  }
+
   return (
-    <div className='addtodo'>
-      <input type='text' placeholder='add new todo' />
-    </div>
+    <form onSubmit={handleSubmit} className='addtodo'>
+      <input className='input' type='text' placeholder='Create a new todo' onChange={(e) => setTodo(e.target.value)} value={todo} />
+      <button className='btn'>Add</button>
+    </form>
   )
 }
 
--- todo-app-redux-toolkit/src/components/TodoList/TodoList.js
+++ todo-app-redux-toolkit/src/components/TodoList/TodoList.js
@@ -1,19 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TodoList.css'
 import cross from '../../images/icon-cross.svg';
 
 const TodoList = ({ todos }) => {
+  // const [activeColor, setActiveColor] = useState(false);
+  const [checked, setChecked] = useState(false);
+
+  const handleClick = () => {
+    console.log('delete');
+  }
+
+  const handleChange = (id, i) => {
+    console.log(id, i)
+    if (id === i) {
+      setChecked(!checked);
+    }
+  }
+
+  console.log(checked);
+
   return (
     <div className='todolist'>
       {
-        todos.map(todo => (
+        todos.map((todo, i) => (
           <div className='todos' key={todo.id}>
-            <li>{todo.todo}</li>
-            <img src={cross} alt='check todos' />
+            <label>
+              <input type='checkbox' checked={checked} onChange={()=>handleChange(todo.id, (i+1))} />
+              <li>{todo.todo}</li>
+            </label>
+            <img src={cross} alt='check todos' onClick={handleClick} />
           </div>
         ))
       }
-      <div>todo</div>
+      <div className='filter'>
+        <span>5 items left</span>
+        <div className='filter-tasks'>
+          <button className='filter-btn active'>All</button>
+          <button className='filter-btn'>Active</button>
+          <button className='filter-btn'>Completed</button>
+        </div>
+        <button className='filter-btn'>Clear Completed</button>
+      </div>
+      <p className='info'>Drag and drop to reorder list</p>
     </div>
   )
 }
